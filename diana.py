56bb34f1000714b5ad90e37b3a7126c39398b32c
dina <baltikovadiana09@gmail.com>
2023-07-03 16:30:27 +0300

    v2

--- diana.py
+++ diana.py
@@ -1,1 +1,24 @@
-print('Helo world')
+from telebot.async_telebot import AsyncTeleBot
+
+bot = AsyncTeleBot('6388793741:AAEeo692qmu59YBm5OjWEey3GE0GGqmsKyc')
+@bot.message_handler(commands = ['help', 'start'])
+async def send_welcome(message):
+    await bot.reply_to(message, 'Приветствую тебя, новый пользователь!')
+
+@bot.message_handler(commands = ['home'])
+async def send_welcome(message):
+    await bot.reply_to(message, 'Привет! Нажми старт, чтобы начать активность бота ')
+@bot.message_handler(func=lambda messange: True)
+async def echo_messange(message):
+    text_message = message.text
+    text_message = text_message.lower()
+    if 'дела' in text_message or 'настроение' in text_message:
+        await bot.reply_to(message, 'хорошо,  у тебя',)
+    elif 'шутка' in text_message or 'анекдот' in text_message:
+        await bot.reply_to(message, 'Колобок повесился')
+    else:
+        await bot.reply_to(message, 'Извините, я вас не поняла')
+
+
+import asyncio
+asyncio.run(bot.polling())
